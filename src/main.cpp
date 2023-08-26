a57bc1c9fa0779bac7a76f5e18fa2319e998c292
portrecistajeden <--list>
2023-08-26 23:07:10 +0200

    encoder and timer library added

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,49 @@
 #include <Arduino.h>
 #include <TM1637Display.h>
+#include <Encoder.h>
+#include <arduino-timer.h>
 
-#define DIO 2 
+#define DIO 2
 #define CLK 3
+#define EncoderCLK 4
+#define EncoderDT 5
 
-TM1637Display display(CLK, DIO);
+TM1637Display tm(CLK, DIO);
+Encoder myEnc(EncoderCLK, EncoderDT);
+Timer<10, millis> timer;
 
-void setup() {
-  // put your setup code here, to run once:
-  display.setBrightness(0x00);
-  display.showNumberDecEx(5);
+int countdownValue = 100;
+int exposureTime = 0;
+void getValueFromEncoder();
+bool countdown(void *);
+
+void setup()
+{
+  tm.setBrightness(0x00);
+  timer.every(1000, countdown);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  timer.tick();
+  if (countdownValue == 0)
+    timer.cancel();
+
+  getValueFromEncoder();
 }
 
+void getValueFromEncoder()
+{
+  int newPosition = myEnc.read();
+  if (newPosition != exposureTime)
+  {
+    exposureTime = newPosition;
+  }
+}
+
+bool countdown(void *)
+{
+  countdownValue--;
+  tm.showNumberDecEx(countdownValue, 0x40, false);
+  return true;
+}
